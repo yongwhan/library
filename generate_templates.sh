7614dd1b897de9d67c7c576ec27012a231b0ac43
Yongwhan Lim <yongwoods@gmail.com>
2019-12-22 21:08:29 -0800

    c++ template file and generate template script

---
+++ generate_templates.sh
@@ -0,0 +1,12 @@
+cp ../template.cpp A.cpp
+cp ../template.cpp B.cpp
+cp ../template.cpp C.cpp
+cp ../template.cpp D.cpp
+cp ../template.cpp E.cpp
+cp ../template.cpp F.cpp
+cp ../template.cpp G.cpp
+cp ../template.cpp H.cpp
+cp ../template.cpp I.cpp
+cp ../template.cpp J.cpp
+cp ../template.cpp K.cpp
+cp ../template.cpp L.cpp
---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef tree<ii,null_type,less<ii>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+
+int main() {
+	cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+	return 0;
+}
